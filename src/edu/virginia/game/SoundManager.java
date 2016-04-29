7dd97d57331c6c5c74e6b1775203d5a38d7655d0
Ra1nWarden <wzh19921016@gmail.com>
2016-04-29 10:52:34 -0400

    Trying to fix sound.

--- src/edu/virginia/game/SoundManager.java
+++ src/edu/virginia/game/SoundManager.java
@@ -4,9 +4,11 @@ import java.io.File;
 
 import javax.sound.sampled.spi.FormatConversionProvider;
 import javax.sound.sampled.spi.AudioFileReader;
-
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 
 import edu.virginia.engine.events.DeathEvent;
 import edu.virginia.engine.events.Event;
@@ -29,10 +31,18 @@ public class SoundManager implements IEventListener {
 
 	static void playMusic(File sound) {
 		try {
-			Clip clip = AudioSystem.getClip();
-			clip.open(AudioSystem.getAudioInputStream(sound));
-
+			AudioInputStream soundIn = AudioSystem.getAudioInputStream(sound);
+			AudioFormat format = soundIn.getFormat();
+			DataLine.Info info = new DataLine.Info(Clip.class, format);
+			Clip clip = (Clip)AudioSystem.getLine(info);
+			clip.open(soundIn);
 			clip.start();
+			
+			
+//			Clip clip = AudioSystem.getClip();
+//			clip.open(AudioSystem.getAudioInputStream(sound));
+
+		//	clip.start();
 			clip.loop(clip.LOOP_CONTINUOUSLY);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
